4693f79a5a604092e380e2b3e026c7f9f8e16f81
Shernay <xerinay85@gmail.com>
2019-01-19 15:10:27 -0500

    commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,7 @@ public class Calculator {
     public static double add (double num1,double num2){
         return num1+num2;
     }
+    public static double subtract(double num1,double num2){
+        return num1-num2;
+    }
 }
