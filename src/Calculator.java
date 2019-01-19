389b24ddc3e3c34c954b2a3cdcd15537a9a0710e
Shernay Sawuthaji <46769495+xerinay@users.noreply.github.com>
2019-01-19 16:39:09 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,7 @@ public class Calculator {
         return num1+num2;
     }
     public static double subtract(double num1,double num2){
-        return num1-num2;
+        result=num1-num2;
+        return result;
     }
 }
